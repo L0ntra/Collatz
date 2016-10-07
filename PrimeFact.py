d2536c9f70883f85d76291bd91401c784912b2fc
L0ntra <ggaston@pdx.edu>
2016-10-07 01:25:40 -0700

    Refactoring Did some refactoring of the algorithm and renaming of some variables Reducing the number of times some values were calculated should improve performances.

--- PrimeFact.py
+++ PrimeFact.py
@@ -1,39 +1,54 @@
-def PrimeFactorize(number,output='p'):
-    "Finds the prime factors of the input number",
-    "output = \'p\', print factorization, \'r\', return divisors list"
-
-    if type(number) != int or number <= 0:
-        print("ERR - PrimeFactorize arg. number")
+from math import sqrt
+def PrimeFactorize(num, output='p'):
+    '''Finds the prime factors of the input number",
+       output = p : print factorization
+              | r : return divisors list
+    '''
+    
+    # Input checking
+    if type(nu) != int or num <= 0:
+        print("ERR - PrimeFactorize arg. num")
         return 'ERR'
-    elif output not in ['p','r']:
+    if output not in ['p','r']:
         print("ERR - PrimeFactorize arg. output")
         return "ERR"
-    elif int(number**(1/2))+1 >= max(Primes):
-        PrimeExtender(int(number**(1/2))+1)
-        PrimeFactorize(number,output)
+
+    num2root = int(sqrt(n))
+    if (num2root + 1) >= max(Primes):
+        PrimeExtender(num2root + 1)
+        PrimeFactorize(num, output) # Recursion forces input checking on known
+                                    # good values.
+                                    # What does this even do?
     else:
-        n = number
+        n = num
         Divisors = []
         ticker = 0
-        while n != 1:
 
-            if int(n**(1/2))+1 < Primes[ticker]:
-                prime = [int(n),1]
-                Divisors.append(prime)
-                n = 1
+        while True:
+            n2root = int(sqrt(n))
+            if (n2root + 1) < Primes[ticker]:
+                Divisors.append([int(n), 1])
+                break; # n is prime or 1 stop looping
                 
             elif n % Primes[ticker] == 0:
-                find = [Primes[ticker],0]
-                while n % Primes[ticker] == 0:
+                found = [Primes[ticker], 0]
+
+                while n % Primes[ticker] == 0: # Factor out the prime
                     n = n / Primes[ticker]
-                    find[1] = find[1] + 1
-                Divisors.append(find)
+                    found[1] = found[1] + 1    # Record the power of the prime
+                
+                Divisors.append(found)         # Add pime to the list...
                 ticker = 0
                 
             else:
                 ticker += 1
         
         if output == 'p':
-            print(str(number)+' ',Divisors)
+            print(str(num)+' ',Divisors)
         else:
             return Divisors
+
+
+ def main():
+   return
+
